f4cb4818e23941f0ff0402ecfa3f952e7ae84835
Sander Orav <sander@play.ee>
2025-09-09 11:36:50 +0300

    Fixed utils Break type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 export type BreakXS = 'xs'; // 4 columns
 export type BreakSM = 'sm' | 'md' | 'lg' | 'xl'; // 12 columns
 
-export type Break = BreakXS | BreakSM | BreakLG;
+export type Break = BreakXS | BreakSM;
 
 export const breakPoints: Record<Break, number> = {
   lg: 1024,
